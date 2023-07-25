54454b486fe6f525e34a73b48db39a95c3ac7a8f
Renzo Battisti <renzo.battisti1@gmail.com>
2023-07-24 22:46:16 -0300

    36 valid sudoku resolvido (>74% em p)(>27% em m)

--- leetcode/medium/36_valid_sudoku/src/main.c
+++ leetcode/medium/36_valid_sudoku/src/main.c
@@ -1,36 +1,96 @@
-#include <stdio.h>
+bool rowCheck(char** board, int row);
+bool columnCheck(char** board, int column);
+bool quadrantCheck(char** board, int _row, int _column);
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello World!\n");
-
-    return 0;
-
-}
 
 bool isValidSudoku(char** board, int boardSize, int* boardColSize){
 
-    char*** board3d = (char***)malloc(9 * sizeof(char**));
-    char** tempMatrix = (char**)malloc(3 * sizeof(char*));
-
-    for(int quadrant = 0; quadrant < 9; quadrant++)
+    for(int i = 0; i < 9; i++)
     {
-        for(int line = 0; line < 3; line++)
+        if(!rowCheck(board, i)) return false;
+        if(!columnCheck(board, i)) return false;
+    }
+
+    for(int _row = 0; _row <= 6; _row+= 3)
+    {
+        for(int _column = 0; _column <= 6; _column+= 3)
         {
-            char* tempArray = (char*)malloc(3 * sizeof(char));
-
-            for(int column = 0; column < 3; column++)
-            {   
-                tempArray[column] = board[line+(3*quadrant/3)][column+(3 * quadrant % 3)];
-            }
-
-            tempMatrix[line] = tempArray[0];
-            free(tempArray); 
+            if(!quadrantCheck(board, _row, _column)) return false;
         }
     }
 
-    for(int i = 0; i < blaba; i++)
-    {
+    return true;
 
+}
+
+bool rowCheck(char** board, int row)
+{
+    int count[9] = {0};
+
+    for(int column = 0; column < 9; column++)
+        {
+            if(board[row][column] >= '1' && board[row][column] <= '9')
+            {
+                count[board[row][column] - '1']++;  
+            }
+        }
+
+    for(int i = 0; i < 9; i++)
+    {
+        if (count[i] != 0 && count[i] != 1)
+        {
+            return false;
+        }
     }
+    return true;
+
+}
+
+bool columnCheck(char** board, int column)
+{
+    int count[9] = {0};
+
+    for(int row = 0; row < 9; row++)
+        {
+            if(board[row][column] >= '1' && board[row][column] <= '9')
+            {
+                count[board[row][column] - '1']++;
+            }
+        }
+    
+    for(int i = 0; i < 9; i++)
+    {
+        if(count[i] != 0 && count[i] != 1)
+        {
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+bool quadrantCheck(char** board, int _row, int _column)
+{
+    int count[9] = {0};
+
+    for(int row = _row; row < _row + 3; row++)
+    {
+        for(int column = _column; column < _column + 3; column++)
+        {
+            if(board[row][column] >= '1' && board[row][column] <= '9')
+                {
+                    count[board[row][column] - '1']++;
+                }
+        }
+    } 
+
+    for(int i = 0; i < 9; i++)
+    {
+        if(count[i] != 0 && count[i] != 1)
+        {
+            return false;
+        }
+    } 
+    return true;
+
 }
