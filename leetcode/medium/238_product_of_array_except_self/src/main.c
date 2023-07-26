81589e6bb2684316ff75cac416d5825bab69dd93
Renzo Battisti <renzo.battisti1@gmail.com>
2023-07-25 21:32:39 -0300

    238 resolvido (>20% em p)(>34% em m)

---
+++ leetcode/medium/238_product_of_array_except_self/src/main.c
@@ -0,0 +1,40 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ const int OFFSET = +30;
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize){
+
+    int* answer = (int*)malloc(sizeof(int) * numsSize);
+    int possibles[61] = {0};
+    int product = 0;
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(possibles[nums[i]+OFFSET] == 0)
+        {
+            for(int c = 0, teste = false; c < numsSize; c++)
+            {
+                if(c != i && teste == true)
+                {
+                    product*= nums[c];
+                } 
+                if(c != i && teste == false)
+                {
+                    product =  nums[c];
+                    teste = true;
+                }
+                
+            }
+            possibles[nums[i]+OFFSET] = product;
+            answer[i] = product;
+            product = 0;
+        }
+        else
+        {
+            answer[i] = possibles[nums[i] + OFFSET];
+        }
+    }
+    *returnSize = numsSize;
+    return answer;
+}
