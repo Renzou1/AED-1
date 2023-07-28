56607834d162625f48e35ade81a2e3736c7ca800
Renzo Battisti <renzo.battisti1@gmail.com>
2023-07-27 22:16:18 -0300

    20_valid_parenthesis resolvido (>49% p)(>49% m)

--- leetcode/easy/20_valid_parenthesis/src/main.c
+++ leetcode/easy/20_valid_parenthesis/src/main.c
@@ -1,161 +1,50 @@
-bool roundBrackets(char* s, int position);
-
-bool squareBrackets(char* s, int position);
-
-bool curlyBrackets(char* s, int position);
-
-
 bool isValid(char * s){
+    char toClear[strlen(s)+2];
+    int top = 0;
 
-    bool output = false;
-    int sLength = strlen(s);
-
-    for(int position = 0; position < sLength; position++)
+    for(int i = 0; i < strlen(s) + 1; i++)
     {
-    
-    switch(s[position])
-    {
-        case '(':
-        output = roundBrackets(s, position);
-        break;
-
-        case '[':
-        output = squareBrackets(s, position);
-
-        case '{':
-        output = curlyBrackets(s, position);
-
-        default:
-        output = false;
+        toClear[i] = '\0';
     }
-        if(output == true)
+
+    for(int i = 0; i < strlen(s); i++)
+    {
+        switch(s[i])
         {
+            case '(':
+            top++;
+            toClear[top] = '(';
+            break;
+
+            case '[':
+            top++;
+            toClear[top] = '[';
+            break;
+
+            case '{':
+            top++;
+            toClear[top] = '{';
+            break;
+
+
+            case ')':
+            if(toClear[top] != '(') return false;
+            toClear[top] = '\0';
+            top--;
+            break;
+
+            case ']':
+            if(toClear[top] != '[') return false;
+            toClear[top] = '\0';
+            top--;
+            break;
+            
+            case '}':
+            if(toClear[top] != '{') return false;
+            toClear[top] = '\0';
+            top--;
             break;
         }
     }
-
-    return output;
-}
-
-bool roundBrackets(char* s, int position)
-{
-    bool output = false;
-    int sLength = strlen(s);
-    bool temp = true;
-
-    for(int c = position; c < sLength; c++)
-    {
-        if(temp == true)
-        {
-        switch (s[c])
-        {
-            
-            case ')':
-            {
-                output = true;
-                break;
-            }
-            
-            case '[':
-            {
-                temp = squareBrackets(s, c);
-                c = c++;
-                break;
-                
-            }
-
-            case '{':
-            {
-                temp = curlyBrackets(s, c);
-                c = c++;
-                break;
-            }
-        }
-        }
-
-
-                
-    }
-    
-    return output;
-}
-
-bool squareBrackets(char* s, int position)
-{
-    int output = false;
-    int sLength = strlen(s);
-    bool temp = true;
-
-    for(int c = position; c < sLength; c++)
-    {
-        if(temp == true)
-        {
-        switch (s[c])
-        {
-            
-            case ']':
-            {
-                output = true;
-                break;
-            }
-            
-            case '(':
-            {
-                temp = roundBrackets(s, c);
-                c = c++;
-                break;
-                
-            }
-
-            case '{':
-            {
-                temp = curlyBrackets(s, c);
-                c = c++;
-                break;
-            }
-        }
-        }
-    }
-
-    return output;
-}
-
-bool curlyBrackets(char* s, int position)
-{
-    int sLength = strlen(s);
-    bool output = false;
-    bool temp = true;
-
-    for(int c = position; c < sLength; c++)
-    {
-        if(temp == true)
-        {
-        switch (s[c])
-        {
-            
-            case '}':
-            {
-                output = true;
-                break;
-            }
-            
-            case '(':
-            {
-                temp = roundBrackets(s, c);
-                c = c++;
-                break;
-                
-            }
-
-            case '[':
-            {
-                temp = squareBrackets(s, c);
-                c = c++;
-                break;
-            }
-        }
-        }
-    }
-
-    return output;
+    return toClear[top] == '\0';
 }
