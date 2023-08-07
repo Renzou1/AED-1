f36321306d18c4554e91b6ede368bb6397195294
Renzo Rossato Battisti <114614401+Renzou1@users.noreply.github.com>
2023-08-07 17:43:54 -0300

    Update main.c

--- leetcode/easy/933_number_of_recent_calls/src/main.c
+++ leetcode/easy/933_number_of_recent_calls/src/main.c
@@ -11,53 +11,58 @@ RecentCounter* recentCounterCreate() {
     RecentCounter* obj = (RecentCounter*)malloc(sizeof(RecentCounter));
     for(int i = 0; i < 3001; i++)
     {
-        obj->requests[i] = 0;
+        obj->requests[i] = false;
     }
 
     obj-> rangeBegin = 0;
-    obj->requestCounter = 0;
+    obj-> requestCounter = 0;
+    obj-> previousRangeBegin = 0;
+
     return obj;
 }
 
 int recentCounterPing(RecentCounter* obj, int t) {
 
+    obj->previousRangeBegin = obj->rangeBegin;
     obj->rangeBegin = t-3000;
-    if(obj->rangeBegin < 0) obj->rangeBegin = 0;
+    if(obj->rangeBegin < 0) obj->rangeBegin = 0; //avoiding negative
     obj->rangeEnd = t;
-    
-    obj->requestCounter++;
 
     int shift = (obj->rangeBegin - obj->previousRangeBegin);
-    
 
-    for(int i = 0; i < shift; i++)
-    {   
-        if(shift > 3000)
-        {
-            if(i == 3001)
-            {
-                obj->requestCounter = 0;
-                break;
-            }
-            obj->requests[i] = false;
-            continue;
-        }
-        if(obj->requests[i] == true)
-        {
-            obj->requests[i]= false;
-            obj->requestCounter--;
-        }   
-    }
-
-    for(int i = shift; i < 3001; i++)
+    if(shift > 3000)
     {
-        if(obj->requests[i] == true)
+        for(int c = 0; c < 3001; c++)
         {
-            obj->requests[i] = false;
-            obj->requests[i-shift] = true;
+            obj->requests[c] = false;
         }
+        obj->requestCounter = 0;
+
+    } else{
+    
+        for(int i = 0; i < shift; i++)
+        {   
+            if(obj->requests[i] == true)
+            {
+                obj->requests[i] = false;
+                obj->requestCounter--;
+            }   
+        }
+    
+        for(int i = shift; i < 3001; i++) 
+        {
+            if(obj->requests[i] == true)
+            {
+                obj->requests[i] = false;
+                obj->requests[i-shift] = true;
+            }
+        }
+
     }
 
+    obj->requests[obj->rangeEnd - obj->rangeBegin] = true;
+    obj->requestCounter++;
+    
     return obj->requestCounter;
 }
 
