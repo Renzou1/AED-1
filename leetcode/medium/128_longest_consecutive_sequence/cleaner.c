c343ec7115994187ba402139c04bd11fbce890ba
Renzo Rossato Battisti <114614401+Renzou1@users.noreply.github.com>
2023-09-28 20:54:44 -0300

    slightly cleaner solution

---
+++ leetcode/medium/128_longest_consecutive_sequence/cleaner.c
@@ -0,0 +1,89 @@
+void merge(const int arr[], int size1, const int arr2[], int size2, int final[])
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while(i < size1 && j < size2)
+    {
+        if(arr[i] < arr2[j])
+        {
+            final[k] = arr[i];
+            i++;
+        }  else{
+            final[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < size1)
+    {
+        final[k] = arr[i];
+        i++;
+        k++;
+    }
+    while(j < size2)
+    {
+        final[k] = arr2[j];
+        j++;
+        k++;
+    }
+
+}
+
+void merge_sort(int arr[], int size)
+{
+    if(size < 2) return;
+    int mid = size/2;
+    int i = 0;
+    int left[mid];
+    for(i = 0; i < mid; i++)
+    {
+        left[i] = arr[i];
+    }
+    int right[size - (i-1)];
+    while(i < size)
+    {
+        right[i - mid] = arr[i];
+        i++;
+    }
+    merge_sort(left, mid);
+    merge_sort(right, size-mid);
+    merge(left, mid, right, size-mid, arr);
+}
+
+
+
+int longestConsecutive(int* nums, int numsSize){
+
+    if(numsSize == 0) return 0;
+
+    merge_sort(nums, numsSize);
+
+    int biggest = 1;
+    int current; 
+    int difference;
+    for(int i = 1; i < numsSize; i++)
+    {
+        current = 1;
+
+        while(i < numsSize)
+        {
+            difference = nums[i] - nums[i-1];
+
+            if(difference <= 1)
+            {
+                current+= difference; 
+                i++;
+
+            }   else break;
+        }
+
+        if(current > biggest)
+        {
+            biggest = current;
+        }
+    }
+
+    return biggest;
+}
